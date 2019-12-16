1b79ed2241ebe2441678db10e9a53d86b3aad031
Michael Steinberg <steinberg.michael@outlook.com>
2019-12-15 20:55:23 -0800

    fix Component error

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,48 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
-import { connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 
-
-const mapStateToProps = state => {
-    return {
-        dishes: state.dishes
-    }
-}
-
+const mapStateToProps = (state) => {
+  return {
+    dishes: state.dishes,
+  };
+};
 
 class Menu extends Component {
-    
-    static navigationOptions = {
-        title: 'Menu'
+  static navigationOptions = {
+    title: 'Menu',
+  };
+
+  render() {
+    const renderMenuItem = ({ item, index }) => {
+      return (
+        <Tile
+          key={index}
+          title={item.name}
+          caption={item.description}
+          featured
+          onPress={() => navigate('DishDetail', { dishId: item.id })}
+          leftAvaimageSrc={{ uri: baseUrl + item.image }}
+        />
+      );
     };
-        
+    const { navigate } = this.props.navigation;
 
-    render(){
-         const renderMenuItem = ({item, index}) => {
-            return (
-                    <Tile
-                        key={index}
-                        title={item.name}
-                        caption={item.description}
-                        featured
-                        onPress={() => navigate('DishDetail', { dishId: item.id })}
-                        leftAvaimageSrc={{ uri: baseUrl + item.image }}
-                      />
-            );
-        }
-        const { navigate } = this.props.navigation;
-
-        return(
-            <FlatList
-            data={this.props.dishes.dishes}
-            renderItem={renderMenuItem}
-            keyExtractor={item => item.id.toString()}
-            />
-        );
-    }
-
+    return (
+      <FlatList
+        data={this.props.dishes.dishes}
+        renderItem={renderMenuItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    );
+  }
 }
 
 export default connect(mapStateToProps)(Menu);
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl = 'http://10.0.0.178:3001/'
+export const baseUrl = 'http://192.168.11.60:3001/';
+// export const baseUrl = 'http://10.0.0.178:3001/'
